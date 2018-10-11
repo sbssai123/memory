593acd8d069cde330c6faca2403386f13297eefc
sbssai123 <sbs.sai@gmail.com>
2018-10-11 14:29:55 -0400

    fixed switching between players

--- lib/memory/game.ex
+++ lib/memory/game.ex
@@ -5,6 +5,7 @@ defmodule Memory.Game do
       current_tiles: [],
       matched: [],
       players: %{},
+      next_player: "",
       }
   end
 
@@ -15,14 +16,12 @@ defmodule Memory.Game do
     Map.put(new(), :players, Enum.into(players, %{}))
   end
 
-
   def default_player() do
     %{
       matches: 0, # of tiles matched
       turn: -1, # 1 is there turn and -1 mean they are a watcher
     }
   end
-
 
   def client_view(game, user) do
     %{
@@ -40,23 +39,28 @@ defmodule Memory.Game do
 
   def add_player(game, player) do
     keys = Map.keys(game.players)
-    cond do
-      length(keys) == 0 ->
-        pinfo = default_player()
-        |> Map.put(:turn, 1)
-        players = Map.put(game.players, player, pinfo)
-        game
-        |> Map.put(:players, players)
-        # |> Map.put(:current_player, player)
-      length(keys) == 1 ->
-        pinfo = default_player()
-        |> Map.put(:turn, 0)
-        players = Map.put(game.players, player, pinfo)
-        Map.put(game, :players, players)
-      true ->
-        pinfo = default_player()
-        players = Map.put(game.players, player, pinfo)
-        Map.put(game, :players, players)
+    if Enum.member?(keys, player) do
+      game
+    else
+      cond do
+        length(keys) == 0 ->
+          pinfo = default_player()
+          |> Map.put(:turn, 1)
+          players = Map.put(game.players, player, pinfo)
+          game
+          |> Map.put(:players, players)
+        length(keys) == 1 ->
+          pinfo = default_player()
+          |> Map.put(:turn, 0)
+          players = Map.put(game.players, player, pinfo)
+          game
+          |> Map.put(:players, players)
+          |> Map.put(:next_player, player)
+        true ->
+          pinfo = default_player()
+          players = Map.put(game.players, player, pinfo)
+          Map.put(game, :players, players)
+      end
     end
   end
 
@@ -66,7 +70,6 @@ defmodule Memory.Game do
     |> Map.get(:turn)
     turn == 0 || turn == -1 || length(keys) < 2
   end
-
 
   def flip_tile(game, player, tile_index) do
     if not_playing(game, player) do
@@ -84,19 +87,29 @@ defmodule Memory.Game do
     if first_tile == second_tile do
       pinfo = Map.get(game.players, player)
       new_pinfo = change_player(pinfo)
-      players = Map.put(game.players, player, new_pinfo)
+      next_pinfo = Map.get(game.players, game.next_player)
+      new_next_pinfo = change_player(next_pinfo)
+      players = game.players
+      |> Map.put(player, new_pinfo)
+      |> Map.put(game.next_player, new_next_pinfo)
       matched = game.matched ++ game.current_tiles
       game
       |> Map.put(:matched, matched)
       |> Map.put(:players, players)
       |> Map.put(:current_tiles, [])
+      |> Map.put(:next_player, player)
     else
       pinfo = Map.get(game.players, player)
       new_pinfo = change_player(pinfo)
-      players = Map.put(game.players, player, new_pinfo)
+      next_pinfo = Map.get(game.players, game.next_player)
+      new_next_pinfo = change_player(next_pinfo)
+      players = game.players
+      |> Map.put(player, new_pinfo)
+      |> Map.put(game.next_player, new_next_pinfo)
       game
       |> Map.put(:current_tiles, [])
       |> Map.put(:players, players)
+      |> Map.put(:next_player, player)
     end
   end
 
